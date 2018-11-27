f6a9a5bb0551ffc9f2503e96ac5b23d55516bdc7
MadJiGit <m_r@abv.bg>
2018-11-27 12:07:03 +0200

    test commit

--- src/FitnessBundle/Controller/DefaultController.php
+++ src/FitnessBundle/Controller/DefaultController.php
@@ -1,9 +1,10 @@
 <?php
 
+// only for test github repo commit
+
 namespace FitnessBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
@@ -11,7 +12,7 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="fitness_index")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig');
